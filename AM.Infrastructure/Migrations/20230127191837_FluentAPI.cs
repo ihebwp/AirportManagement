456e31da2200ef7996b3cf1fb3504e96ed1e1c18
iheb Mehouachi <88165132+ihebwp@users.noreply.github.com>
2023-02-06 15:45:28 +0100

    Add files via upload

---
+++ AM.Infrastructure/Migrations/20230127191837_FluentAPI.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Infrastructure.Migrations
+{
+    public partial class FluentAPI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FlightPassenger_Flights_FlightsFlightId",
+                table: "FlightPassenger");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FlightPassenger_Passengers_PassengersPassportNumber",
+                table: "FlightPassenger");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flights_Planes_PlaneFK",
+                table: "Flights");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Planes",
+                table: "Planes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FlightPassenger",
+                table: "FlightPassenger");
+
+            migrationBuilder.RenameTable(
+                name: "Planes",
+                newName: "MyPlanes");
+
+            migrationBuilder.RenameTable(
+                name: "FlightPassenger",
+                newName: "VolsVoyageurs");
+
+            migrationBuilder.RenameColumn(
+                name: "Capacity",
+                table: "MyPlanes",
+                newName: "PlaneCapacity");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_FlightPassenger_PassengersPassportNumber",
+                table: "VolsVoyageurs",
+                newName: "IX_VolsVoyageurs_PassengersPassportNumber");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyPlanes",
+                table: "MyPlanes",
+                column: "PlaneId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VolsVoyageurs",
+                table: "VolsVoyageurs",
+                columns: new[] { "FlightsFlightId", "PassengersPassportNumber" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flights_MyPlanes_PlaneFK",
+                table: "Flights",
+                column: "PlaneFK",
+                principalTable: "MyPlanes",
+                principalColumn: "PlaneId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VolsVoyageurs_Flights_FlightsFlightId",
+                table: "VolsVoyageurs",
+                column: "FlightsFlightId",
+                principalTable: "Flights",
+                principalColumn: "FlightId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VolsVoyageurs_Passengers_PassengersPassportNumber",
+                table: "VolsVoyageurs",
+                column: "PassengersPassportNumber",
+                principalTable: "Passengers",
+                principalColumn: "PassportNumber",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flights_MyPlanes_PlaneFK",
+                table: "Flights");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VolsVoyageurs_Flights_FlightsFlightId",
+                table: "VolsVoyageurs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VolsVoyageurs_Passengers_PassengersPassportNumber",
+                table: "VolsVoyageurs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VolsVoyageurs",
+                table: "VolsVoyageurs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyPlanes",
+                table: "MyPlanes");
+
+            migrationBuilder.RenameTable(
+                name: "VolsVoyageurs",
+                newName: "FlightPassenger");
+
+            migrationBuilder.RenameTable(
+                name: "MyPlanes",
+                newName: "Planes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VolsVoyageurs_PassengersPassportNumber",
+                table: "FlightPassenger",
+                newName: "IX_FlightPassenger_PassengersPassportNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "PlaneCapacity",
+                table: "Planes",
+                newName: "Capacity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FlightPassenger",
+                table: "FlightPassenger",
+                columns: new[] { "FlightsFlightId", "PassengersPassportNumber" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Planes",
+                table: "Planes",
+                column: "PlaneId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FlightPassenger_Flights_FlightsFlightId",
+                table: "FlightPassenger",
+                column: "FlightsFlightId",
+                principalTable: "Flights",
+                principalColumn: "FlightId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FlightPassenger_Passengers_PassengersPassportNumber",
+                table: "FlightPassenger",
+                column: "PassengersPassportNumber",
+                principalTable: "Passengers",
+                principalColumn: "PassportNumber",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flights_Planes_PlaneFK",
+                table: "Flights",
+                column: "PlaneFK",
+                principalTable: "Planes",
+                principalColumn: "PlaneId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ AM.Infrastructure/Migrations/20230204143640_tph.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Infrastructure.Migrations
+{
+    public partial class tph : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Passengers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EmployementDate",
+                table: "Passengers",
+                type: "Date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Passengers",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PassengerType",
+                table: "Passengers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ManifactureDate",
+                table: "MyPlanes",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FlightDate",
+                table: "Flights",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EffectiveArrival",
+                table: "Flights",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PassengerType",
+                table: "Passengers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EmployementDate",
+                table: "Passengers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "Date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Passengers",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ManifactureDate",
+                table: "MyPlanes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FlightDate",
+                table: "Flights",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EffectiveArrival",
+                table: "Flights",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+        }
+    }
+}
